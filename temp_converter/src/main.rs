5e2523eb0fe220abacbdbcb0048f304fcd55f45c
Syed Navid Nowroz Twoki <navidnowroz.s@gmail.com>
2026-07-22 01:55:33 +0600

    get_temp and main implemented

--- temp_converter/src/main.rs
+++ temp_converter/src/main.rs
@@ -1,13 +1,27 @@
-use std::io;
+use std::io::{self, Write};
+
+const FTC: &str = "Fahrenheit to Celsius";
+const CTF: &str = "Celsius to Fahrenheit";
 
 fn main() {
-    let mode = Fahrenheit_Or_Celsius();
+    let mode = fahrenheit_or_celsius();
     println!("Selected mode: {mode}");
+    match mode.trim() {
+        FTC => {
+            let temperature: &64 = get_temp();
+            fahrenheit_to_celsius(temperature);
+        },
+        CTF => {
+            let temperature: &64 = get_temp();
+            celsius_to_fahrenheit(temperature);
+        }
+    }
 }
 
-fn Fahrenheit_Or_Celsius() -> &'static str{
+// Gets the mode to run the program on
+fn fahrenheit_or_celsius() -> &'static str{
     // Printing the header info
-    println!("This is a simple rust based app to convert temperature developed by Twoki.")
+    println!("This is a simple rust based app to convert temperature developed by Twoki.");
     println!("Give the number 1 as input if you want to convert from Fahrenheit to Celsius.");
     println!("Give the number 2 as input if you want to convert from Celsius to Fahrenheit.");
 
@@ -24,12 +38,34 @@ fn Fahrenheit_Or_Celsius() -> &'static str{
         let trimmed_input = input.trim();
         if valid_inputs.contains(&trimmed_input) {
             return match trimmed_input {
-                "1" => "Fahrenheit to Celsius",
-                "2" => "Celsius to Fahrenheit",
+                "1" => FTC,
+                "2" => CTF,
                 _ => unreachable!(),
-            };ß
+            };
         } else {
             println!("Invalid input. Please try again.");
         }
     }
+}
+
+// Gets the temperature to convert from
+fn get_temp() -> f64 {
+    let mut input = String::new();
+    let number: f64;
+    println!("Give the temperature you want to convert from as input");
+
+    loop {
+        io::stdout().flush().unwrap();
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        match input.trim().parse::<f64>() {
+            Ok(num) => {
+                number = num;
+                break;
+            }
+            Err(_) => println!("Invalid temperature. Please enter a valid decimal number."),
+        }
+    }
+    number
 }
