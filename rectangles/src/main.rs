586c51e9f2af2448c45a3f5aff4e426c6732f124
Syed Navid Nowroz Twoki <navidnowroz.s@gmail.com>
2026-07-26 01:47:35 +0600

    structs done

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -14,7 +14,7 @@ impl Rectangle {
     }
 }
 fn main() {
-    let rect1 = Rectangle {
+    let rect: Rectangle    = Rectangle {
         height: 30,
         width: 50
     };
@@ -27,8 +27,8 @@ fn main() {
     let rect3 = Rectangle {
         height: 60,
         width: 45,
-    }
+    };
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(rect3));
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
