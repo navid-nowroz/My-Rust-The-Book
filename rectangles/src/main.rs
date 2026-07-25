5cd9989364f66d99dad3547b3ed42cc4e0d92fd7
Syed Navid Nowroz Twoki <navidnowroz.s@gmail.com>
2026-07-25 15:56:53 +0600

    1st area

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    println!("The are of a rectangle is {} square pixels.", area(width1, height1));
+}
+
+fn area(width: u64, height: u64) -> u64 {
+    width * height
 }
