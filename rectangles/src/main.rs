9b567cce58e7695f30f0dab82017a9a85f349cf0
Syed Navid Nowroz Twoki <navidnowroz.s@gmail.com>
2026-07-25 19:06:21 +0600

    5-15

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,12 +8,27 @@ impl Rectangle {
     fn area(&self) -> usize {
         self.height * self.width
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height && self.width > other.width 
+    }
 }
 fn main() {
     let rect1 = Rectangle {
         height: 30,
         width: 50
     };
+    
+    let rect2 = Rectangle {
+        height: 10,
+        width: 40,
+    };
 
-    println!("The are of a rectangle is {} square pixels.", rect1.area());
+    let rect3 = Rectangle {
+        height: 60,
+        width: 45,
+    }
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(rect3));
 }
