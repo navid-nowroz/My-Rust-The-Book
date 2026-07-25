803f72d740e93a5ca46d1bad4491ffdf720e43d1
Syed Navid Nowroz Twoki <navidnowroz.s@gmail.com>
2026-07-26 03:59:09 +0600

    practice done

--- practice_mini_goblin/src/main.rs
+++ practice_mini_goblin/src/main.rs
@@ -1,9 +1,50 @@
+#[derive(Debug)]
 struct Character {
-    // Add fields here!
+    name: String,
+    health: u32,
+    max_health: u32,
+    level: u32,
 }
 
 impl Character {
-    // Add your associated function (new) and methods here!
+    fn new(name: &str) -> Character {
+        Character {
+            name: name.to_string(),
+            health: 100,
+            max_health: 100,
+            level: 1,
+        }
+    }
+
+    fn take_damage(&mut self, ammount: u32) {
+        self.health = self.health.saturating_sub(ammount);
+    }
+
+    fn heal(&mut self, ammount: u32) {
+        let mola: u32 = self.health + ammount;
+        if mola < self.max_health {
+            self.health = mola;
+        }
+        else {
+            self.health = self.max_health;
+        }
+    }
+
+    fn level_up(&mut self) {
+        self.level += 1;
+        self.max_health += 20;
+        self.health = self.max_health;
+    }
+
+    fn print_stats(&self) {
+        println!(
+            "Hero: {n} (Level {l}) Health: {h}/{mh}.",
+            n = self.name,
+            l = self.level,
+            h = self.health,
+            mh = self.max_health,
+        );
+    }
 }
 
 fn main() {
