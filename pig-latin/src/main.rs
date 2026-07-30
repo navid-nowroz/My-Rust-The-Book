721bb9573f249d3faec6b1f61f6463c99bb2e7cd
Syed Navid Nowroz Twoki <navidnowroz.s@gmail.com>
2026-07-30 10:39:24 +0600

    piggigy added

---
+++ pig-latin/src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+
+fn main() {
+
+}
+
+fn get_string() -> String {
+    println!("Please enter the string that you want to convert here. ");
+    let mut input :String = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input
+}
+
+fn piggify(string :&String) -> String {
+    let list :Vec<char> = string.chars().collect();
+    let vowels :Vec<char>= vec!['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+    if vowels.contains(&list[0]) {
+        let sentence :String = list.into_iter().collect();
+        let sentence:String = sentence + "-hay";
+        sentence
+    } else {
+        let slice:&[char] = &list[1..];
+        let pig :String = slice.into_iter().collect();
+        let some :String = list[0].to_string();
+        let pig:String = pig + "-" + &some + "ay";
+        pig
+    }
+}
